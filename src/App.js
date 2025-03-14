3565150781db546a2c3ec49224ef6f5d5652a206
shreecom <harshadaspatil1999@gmail.com>
2025-03-14 05:18:48 -0700

    second

--- src/App.js
+++ src/App.js
@@ -13,16 +13,16 @@ import Filter from './component/home/Filter';
 function App() {
   return (
     <div className="App">
-       <Navbar/>
-       <Routes>
-        <Route extact path='/' element={<Home/>}></Route>
-       <Route path='/product' element={<Product/>}></Route> 
-        <Route path='/product/:id' element={<ProductDetails/>}></Route>
-        
-        <Route path='/contact' element={<Contact/>}></Route>
-        <Route path='/cart' element={<Cart/>}></Route>
-        <Route path='/filter' element={<Filter/>}> </Route>
-            </Routes>
+      <Navbar />
+      <Routes>
+        <Route extact path='/' element={<Home />}></Route>
+        <Route path='/product' element={<Product />}></Route>
+        <Route path='/product/:id' element={<ProductDetails />}></Route>
+
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/cart' element={<Cart />}></Route>
+        <Route path='/filter' element={<Filter />}> </Route>
+      </Routes>
     </div>
   );
 }
