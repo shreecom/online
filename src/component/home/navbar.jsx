30f5b6adb50ec77900b367b67ab187d47f28ae53
shreecom <harshadaspatil1999@gmail.com>
2025-03-14 05:28:43 -0700

    first

--- src/component/home/navbar.jsx
+++ src/component/home/navbar.jsx
@@ -8,33 +8,33 @@ import { FaBars } from "react-icons/fa";
 
 
 function Navbar() {
-  const [isopen,setIsOpen]=useState(false);
-  const toggleMenu=()=>{
+  const [isopen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
     setIsOpen(!isopen);
   }
   const cartItem = useSelector((state) => state.cart.cartItem);
-    return (
+  return (
     <>
-    <div className='nav'>
-      <h2>Fation Market</h2>
-      <div className={isopen ? "linkBar active" : "linkBar"}>
-      {/* <NavLink to='/' className='companyName'> Fation Market</NavLink> */}
-        <NavLink to='/' className='linkname'>Home</NavLink>
-        <NavLink to='/product' className='linkname'>Product</NavLink> 
-        <NavLink to='/about' className='linkname'> About</NavLink> 
-        <NavLink to='/contact' className='linkname'>Contact</NavLink>
-      </div>
+      <div className='nav'>
+        <h2>Fation Market</h2>
+        <div className={isopen ? "linkBar active" : "linkBar"}>
+          {/* <NavLink to='/' className='companyName'> Fation Market</NavLink> */}
+          <NavLink to='/' className='linkname'>Home</NavLink>
+          <NavLink to='/product' className='linkname'>Product</NavLink>
+          <NavLink to='/about' className='linkname'> About</NavLink>
+          <NavLink to='/contact' className='linkname'>Contact</NavLink>
+        </div>
         <div className='navbarBtn'>
-        <NavLink to='/cart' className='btn'>    
-         <ShoppingCartIcon></ShoppingCartIcon>  Cart  {cartItem.length}</NavLink>
-         
-       <NavLink to='/filter' className='btn'> <FiFilter/>  Filter</NavLink>
+          <NavLink to='/cart' className='btn'>
+            <ShoppingCartIcon></ShoppingCartIcon>  Cart  {cartItem.length}</NavLink>
+
+          <NavLink to='/filter' className='btn'> <FiFilter />  Filter</NavLink>
         </div>
         <div className='icon' onClick={toggleMenu}>
-        <FaBars />
+          <FaBars />
         </div>
       </div>
-    
+
     </>
   )
 }
