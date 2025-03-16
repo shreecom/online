79bf487a3e7ba145faaf6d48de79ab3cc0ea69c7
shreecom <harshadaspatil1999@gmail.com>
2025-03-16 10:45:15 -0700

    third

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <BrowserRouter basename="github.io/online/">
+  <BrowserRouter basename="/online">
   <Provider store={store}>
       <App />
       </Provider>
